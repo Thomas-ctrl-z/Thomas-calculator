22fce51d94a286d1cff06c68238616c45ed678d9
Jaeden Souden <jaedensouden@gmail.com>
2025-11-17 05:15:15 -0500

    Buttons display their values Spaces between numbers and special characters

--- calculator.js
+++ calculator.js
@@ -2,6 +2,11 @@ const deniedInput = document.getElementById("input-output");
 const keyStrokes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", 
     "+", "-", "Backspace", "%", "x", "=", ")", "(", ".", " "
 ]
+const input = document.getElementById("input-output");
+
+let button = document.getElementsByTagName("button")
+
+
 
 deniedInput.addEventListener("keydown", function(event) {
     const keyName = event.key
@@ -13,5 +18,25 @@ deniedInput.addEventListener("keydown", function(event) {
     }
 });
 
-//If key press is not equal to the item in the area then invalidate
+for (let i = 0; i < button.length; i++) {
+
+    button[i].addEventListener('click', function(e) {
+        const text = button[i].innerText;
+         let numbers = ["1","2","3","4","5","6","7","8","9","0"];
+         const existing = input.value
+
+         if (text === "clear") {
+            input.value = "";
+
+         } else if (text === "=") {
+            console.log(existing);
+
+         }  else if (!numbers.includes(text)) {
+            input.value += " " + text + " ";
+
+         } else
+            input.value += text;
+    });
+
+}
 
